d969f97d9c54b7471f2a5517d3acfbc947090229
kangwang <2818815189@qq.com>
2025-04-12 17:52:02 +0800

    传输台

---
+++ Assets/script/Container/DeliveryCounter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : ClearCounter
+{
+    public override void Interact(Player player)
+    {
+        KitchObjectController kitchen =  player.GetKitchObjectController();
+        if (kitchen != null && kitchen.TryGetComponent<PlateKitchenObject>(out PlateKitchenObject plateKitchenObject)) {
+            //上到菜是否正确
+            player.OnDestroyKitchen();
+            //销毁
+
+        }
+    }
+}
